e11a6a405d89ec0d5592d8fa1d92e737b014bd8d
ame0244 <asilelsharafi@my.unt.edu>
2025-03-20 14:48:46 -0500

    integrated more comments into receiving file + removed some extraneous include statements

--- project1.c
+++ project1.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <conio.h>
 #include <tchar.h>
-#include <windows.h>
-#include <wincrypt.h>
+//#include <windows.h>
+//#include <wincrypt.h>
 //need 2 include wolfSSL libraries and edit this list as necessary
 
 //initialize functions here
--- project1.py
+++ project1.py
@@ -4,10 +4,18 @@ from Crypto.Hash import SHA256
 from Crypto.PublicKey import ECC
 from Crypto.Signature import DSS
 
+#function to receive public key + message
+
+#intialize key val and import public key thorugh file
 key = ECC.import_key(open('pubkey.der').read())
+
+#create new hash of message
 h = SHA256.new(received_message)
+
+#generate the DSS verification using key
 verifier = DSS.new(key, 'fips-186-3')
 
+#function to match signatures
 try:
     verifier.verify(h, signature)
     print("The message is authentic.")
